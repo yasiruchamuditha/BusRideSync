fcc7e3db202695e2f97eb4917a479f64c7893bc0
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2025-01-03 22:25:18 +0530

    Make bus registration input field read-only

--- src/pages/save_bus.js
+++ src/pages/save_bus.js
@@ -179,6 +179,7 @@ export default function RegisterBus() {
             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             required
             min="1"
+            readOnly
           />
         </div>
 
